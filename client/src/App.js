1b1acfbe82457b6caf87a4b786d261ded0c8bcd4
Orjan Vesterlid <orjanbostad.vesterlid@gmail.com>
2019-08-17 03:49:03 +0200

    Start kubernetes section

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
           >
-	   Fibonacci calculator
+	   Fibonacci calculator version 2
           </a>
 	  <Link to="/">Home</Link>
 	  <Link to="/otherpage">Other Page</Link>
